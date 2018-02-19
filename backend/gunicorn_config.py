5601fbc025263ab17969816a082dcc0641f8d1a9
dmigo <d.f.goryunov@gmail.com>
2018-02-19 04:56:06 +0100

    Reduce amount of workers

--- backend/gunicorn_config.py
+++ backend/gunicorn_config.py
@@ -2,6 +2,6 @@ from multiprocessing import cpu_count
 
 bind = '0.0.0.0:8000'
 worker_class = 'gevent'
-workers = 2 * cpu_count() + 1
+workers = 1 #2 * cpu_count() + 1
 accesslog = '-'
 errorlog = '-'
