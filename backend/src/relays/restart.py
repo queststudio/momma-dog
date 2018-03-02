7e9946273bc6d2fbf593548ab90145063b64d1d8
dmigo <d.f.goryunov@gmail.com>
2018-03-02 23:42:07 +0100

    i2c fix

--- backend/src/relays/restart.py
+++ backend/src/relays/restart.py
@@ -2,5 +2,5 @@ from src.relays.set_state import set_state
 
 
 def restart(): # ToDo move that to some kind of settings
-    set_state(52, 3, False)
     set_state(52, 3, True)
+    set_state(52, 3, False)
