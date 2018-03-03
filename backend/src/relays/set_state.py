89b3db0b79cbd937cfb9c48b69644ab53d3f1cef
dmigo <d.f.goryunov@gmail.com>
2018-03-03 06:20:25 +0100

    Revert "i2c fix"
    This reverts commit 7e9946273bc6d2fbf593548ab90145063b64d1d8.

--- backend/src/relays/set_state.py
+++ backend/src/relays/set_state.py
@@ -10,14 +10,13 @@ class PCF:
         if not self.pcfs.get(address):
             try:
                 from pcf8574 import PCF8574
+                pcf = PCF8574(i2c_port_num, address)
+                pcf.port[port] = state
                 self.pcfs[address] = PCF8574(i2c_port_num, address)
             except ImportError:
                 print('Couldn\'t find module pcf8574')
             except IOError:
                 print('I2C not available')
-
-        if self.pcfs.get(address):
-            self.pcfs.get(address).port[port] = state
 
 
 singleton = PCF()
