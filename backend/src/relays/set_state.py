e9efc70b366e7ac8809174842524b08b41c22368
dmigo <d.f.goryunov@gmail.com>
2018-03-11 23:09:51 +0100

    Split set_state and render

---
+++ backend/src/relays/set_state.py
@@ -0,0 +1,12 @@
+from src.game.models import LockState, Lock, State, SwitchState, Switch
+from src.relays.service import RelayService
+from src.relays.factory import RelayFactory, create_pcf
+
+# ToDo use logger
+factory = RelayFactory(1, create_pcf)
+service = RelayService(factory)
+
+
+def set_state(address, port, state):
+    print('Setting state: ' + str(address) + ' ' + str(port) + ' ' + str(state))
+    service.set_state(address, port, state)
