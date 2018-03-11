7b4fa2f7b333c1b5ee3aee13a66a3f7a1526152c
dmigo <d.f.goryunov@gmail.com>
2018-03-12 00:09:41 +0100

    Fix restart action

--- backend/src/relays/restart.py
+++ backend/src/relays/restart.py
@@ -3,7 +3,7 @@ def restart_creator(set_state):
         # ToDo move that to some kind of settings
         address = 58
         port = 4
-        
+
         set_state(address, port, False)
         set_state(address, port, True)
 
