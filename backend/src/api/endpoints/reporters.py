411a4f4c2d12a8907007e646dedd181bf8f549a1
dmigo <d.f.goryunov@gmail.com>
2018-03-12 00:10:59 +0100

    Setup dependencies

--- backend/src/api/endpoints/reporters.py
+++ backend/src/api/endpoints/reporters.py
@@ -1,11 +1,13 @@
 from flask_restful import Resource, request
 
-from src.game.store import store
 from src.game.models import Puzzle, PuzzleState
 from src.game.actions import UpdatePuzzleAction
 from src.game.queries import ReporterExistsQuery
 
 class Reporters(Resource):
+    def __init__(self, store):
+        self.store = store
+
     def put(self, reporter: str):
         devices = request.json.get('devices')
 
@@ -15,7 +17,7 @@ class Reporters(Resource):
         print('[{}] {}'.format(reporter, devices)) # ToDo use logger
 
         query = ReporterExistsQuery(reporter)
-        reporter_exists = store.perform_query(query)
+        reporter_exists = self.store.perform_query(query)
         if not reporter_exists:
             return None, 404
 
@@ -23,6 +25,6 @@ class Reporters(Resource):
             puzzle = Puzzle(reporter, device['address'])
             puzzle.state = PuzzleState(device['state'])
             command = UpdatePuzzleAction(puzzle)
-            store.act(command)
+            self.store.act(command)
 
         return None, 200
--- backend/src/api/endpoints/switches.py
+++ backend/src/api/endpoints/switches.py
@@ -1,15 +1,17 @@
 from flask_restful import Resource, request
 
-from src.game.store import store
 from src.game.models import SwitchState, Switch as SwitchModel
 from src.game.actions import UpdateSwitchAction
 from src.game.queries import SwitchesQuery, SwitchExistsQuery
 
 
 class Switches(Resource):
+    def __init__(self, store):
+        self.store = store
+
     def get(self):
         query = SwitchesQuery()
-        switches = store.perform_query(query)
+        switches = self.store.perform_query(query)
         result = {
             'switches': [switch.serialize() for switch in switches]
         }
@@ -17,17 +19,20 @@ class Switches(Resource):
 
 
 class Switch(Resource):
+    def __init__(self, store):
+        self.store = store
+
     def put(self, id: int):
         new_state = request.json.get('state')
         if not new_state:
             return 400
 
         query = SwitchExistsQuery(id)
-        switch_exists = store.perform_query(query)
+        switch_exists = self.store.perform_query(query)
 
         if (switch_exists):
             command = UpdateSwitchAction(id, SwitchState(new_state))
-            store.act(command)
+            self.store.act(command)
             return None, 200
         else:
             return None, 404
