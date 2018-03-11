305aa67ab3ede1e7f708217fc5a7bea7206cb487
dmigo <d.f.goryunov@gmail.com>
2018-03-11 22:58:20 +0100

    Games endpoint

---
+++ backend/src/api/endpoints/games.py
@@ -0,0 +1,30 @@
+from flask_restful import Resource, request
+
+from src.game.store import store, init_state
+from src.game.actions import RestartAction
+from src.game.queries import GameQuery
+from src.relays.restart import restart
+
+
+class CurrentGame(Resource):
+    def get(self):
+        query = GameQuery()
+        current_game = store.perform_query(query)
+        result = {
+            'game': current_game
+        }
+        return result, 200
+
+
+    def post(self):
+        action = RestartAction(init_state)
+        store.act(action)
+
+        #ToDo decide if I really need this part here
+        query = GameQuery()
+        current_game = store.perform_query(query)
+        result = {
+            'game': current_game
+        }
+
+        return result, 200
